9a4061a2b5091e0ad2143d69306d9f6b412432e9
Josue Zamudio <jozamudi@slac.stanford.edu>
2024-03-27 14:05:12 -0700

    Added the ability for the test server to process commands from the suported.json file and responds with a predefined template, arguments passed though the http request are subtituted into the template.

--- testServer.py
+++ testServer.py
@@ -4,30 +4,49 @@ import json
 
 hostName = "localhost"
 serverPort = 8081
-capabilities = dict()
+supportedJson = 'supported.json'
+
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self): 
 
-        try:
-            payLoad = capabilities[self.path];
-        except:
-            self.send_response(404)
+        # Parse command
+        parsePath = self.path.split('?')
+        command = parsePath.pop(0)
+        args = parsePath
 
+        print(command, args)
+
+        # Check if command exists
+        fd = open(supportedJson)
+        data = json.load(fd)
+        capabilities = dict()
+        for endpoint in data['endpoints']:
+            capabilities[endpoint["API"]] = endpoint["payload"]
+
+        fd.close()
+
+        if command not in capabilities.keys():
+            self.send_response(404)
+            return
+
+
+        # Respond to client
         self.send_response(200)
+        payLoad = capabilities[command];
+
+        # Replace args
+        for item in args:
+            var = item.split('=')
+            payLoad = payLoad.replace(var[0], var[-1])
+
+
+        print(payLoad)
         self.wfile.write(bytes(payLoad, "utf-8"))
 
 
 
 if __name__ == "__main__":        
-
-    fd = open('supported.json')
-    data = json.load(fd)
-    for endpoint in data['endpoints']:
-        capabilities[endpoint["API"]] = endpoint["payload"]
-    fd.close()
-
-    print(capabilities)
 
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
