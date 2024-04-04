cba6db8cff1fdd935841337575f876958eded87c
Josue Zamudio <jozamudi@DESKTOP-T575UM9>
2024-04-04 13:18:49 -0700

    I grabbed sample output from all of the current api endpoints. The test server now responds same way as the dod robot. Works with browser

--- testServer.py
+++ testServer.py
@@ -2,13 +2,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+
 hostName = "localhost"
 serverPort = 8081
 supportedJson = 'supported.json'
 
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self): 
+    def do_GET(self):
 
         # Parse command
         parsePath = self.path.split('?')
@@ -28,25 +29,24 @@ class MyServer(BaseHTTPRequestHandler):
 
         if command not in capabilities.keys():
             self.send_response(404)
+            self.end_headers()
             return
 
-
         # Respond to client
-        self.send_response(200)
         payLoad = capabilities[command];
+        header = data['header']
+        header['Result'] = payLoad
 
-        # Replace args
-        for item in args:
-            var = item.split('=')
-            payLoad = payLoad.replace(var[0], var[-1])
-
-
-        print(payLoad)
+        # Response Json
+        payLoad = json.dumps(header,ensure_ascii=False)
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
         self.wfile.write(bytes(payLoad, "utf-8"))
 
 
 
-if __name__ == "__main__":        
+if __name__ == "__main__":
 
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
