50f39eeb242dcf313069565a91b4d8db71366e97
Josue Zamudio <55956919+jozamudi@users.noreply.github.com>
2024-05-08 15:58:01 -0700

    Created a helper object used to parse response from Robot, see issue #5 (#6)
    * Created a helper object used to parse response from Robot, see issue #5
    
    * Added Endpoints handler class to reload, keep track of endpoints capablilites and update json file ? (TODO).
    
    * Fixed reload_endpoint
    
    * fixed logging on helpers.py
    
    * Add logging shows where the log message is comming from
    
    * myClient wraps helper send and get funtions
    
    * chore(cleanup): keeping duplicate code to minimum, some whitespace space things
    
    ---------
    
    Co-authored-by: josh-deb <joshc@slac.stanford.edu>

--- drops/DropsDriver.py
+++ drops/DropsDriver.py
@@ -1,10 +1,14 @@
 import logging
-import json
 import pprint
 import argparse
 
 from http.client import HTTPConnection
 from multiprocessing import Queue, Semaphore
+from helpers import ServerResponse, SupportedEndsHandler
+from helpers import send as h_send
+from helpers import get_response as h_get_response
+
+logger = logging.getLogger(__name__)
 
 
 def parse_arguments(obj):
@@ -15,10 +19,10 @@ def parse_arguments(obj):
 
   group = parser.add_mutually_exclusive_group(required=True)  # only accept one of the following
   # get, trivial
-  group.add_argument("-g", "--get", help="Call HTTP get on endpoint", type=str, choices=obj.supported_ends['get'])
+  group.add_argument("-g", "--get", help="Call HTTP get on endpoint", type=str, choices=obj.supported_ends()['get'])
   # do
-  group.add_argument("-m", "--move", help="move to enumerated position", type=str, choices=obj.supported_ends['do']["/DoD/do/Move?PositionName={value}"])
-  group.add_argument("-t", "--task", help='execute enumerated task', type=str, choices=obj.supported_ends['do']["/DoD/do/ExecuteTask?TaskName={value}"])
+  group.add_argument("-m", "--move", help="move to enumerated position", type=str, choices=obj.supported_ends()['do']["/DoD/do/Move?PositionName={value}"])
+  group.add_argument("-t", "--task", help='execute enumerated task', type=str, choices=obj.supported_ends()['do']["/DoD/do/ExecuteTask?TaskName={value}"])
 
   return parser.parse_args()
 
@@ -27,7 +31,7 @@ def parse_arguments(obj):
 Simple HTTP Client wrapper.
 Takes ip and port on construction can send data on socket and print reply
 Can be invoked via command line args or as orchestrated by higher level software
-''' 
+'''
 
 
 class myClient:
@@ -37,50 +41,13 @@ class myClient:
     self.__queue__ = Queue()
     self.__queue_ready__ = Semaphore(value=0)
 
-    self.enumerate_ends()  # expensive file io, do once in constructor. 
-
     self.conn = HTTPConnection(host=self.__IP__, port=self.__PORT__)
-    logging.info(f"Connected to ip: {ip} port: {port}")
+    self.supported_ends_handler = SupportedEndsHandler('supported.json',
+                                                        self.conn)
 
-    self.enumerate_valid_dos()
-  
-  '''
-  TODO: I would like these moved to an 'update fiducual' section, to be persisted in a source of truth json file
-        the object can then decide to load the held fiducal list or generate a new one via these calls
-  '''
-  def enumerate_valid_dos(self):
-    for ent in self.supported_ends['do'].keys():
-      # check this do endpoint takes an argument
-      if '?' in ent:
-        # Any float is acceptable for pure moves
-        if 'MoveX' in ent or 'MoveY' in ent or "MoveZ" in ent:
-          continue
-        # slightly evil but this queries for supported argument lists per do end point
-        cursed = f"/DoD/get/{ent.split('?')[1].split('=')[0]}s"
-        print(f"Cursed endpoint: {cursed}")
-        self.send(cursed)
-        self.supported_ends['do'][ent] = json.loads(self.get_response().read().decode('utf-8'))["Result"]  # i should be shot for this
-
-    pprint.pprint(self.supported_ends)
-
-  def enumerate_ends(self):
-    self.supported_ends = {
-      'get' : [],
-      'do' : {},
-      'conn' : []
-    }
-    # attempt to ingest supported.json which enumerates API end points
-    try:
-      f = open('supported.json')
-    except FileNotFoundError:
-      logging.error("File supported.json not found")
-    with f:
-      json_data = json.load(f)["endpoints"]
-      self.supported_ends['get'] = [x['API'] for x in json_data if x['API'][5:8] == 'get']
-      self.supported_ends['do'] = {x['API'] : None for x in json_data if x['API'][5:7] == 'do'}
-      self.supported_ends['conn'] = [x['API'] for x in json_data if 'connect' in x['API'][5:].lower()]
-
-    pprint.pprint(self.supported_ends)
+    logger.info(f"Connected to ip: {ip} port: {port}")
+    self.supported_ends = lambda : self.supported_ends_handler.get_endpoints()
+    pprint.pprint(self.supported_ends())
 
   '''
   send transmits a formatted HTTP GET request
@@ -88,24 +55,14 @@ class myClient:
   it will persist result in a place that can be read... TODO: actually do this
   '''
   def send(self, endpoint):
-    logging.info(f"attempting to send: {endpoint}...")
-    self.conn.request("GET", endpoint)
-    logging.info("issued request")
-    reply = self.conn.getresponse()
-    logging.info("got response")
-
-    if (self.__queue__ is not None):
-      self.__queue__.put(reply)
-      self.__queue_ready__.release()
-    return
+      h_send(self.conn, self.__queue__, self.__queue_ready__, endpoint)
+      return
 
   '''
   Pops most recent response from response queue
   '''
   def get_response(self):
-    self.__queue_ready__.acquire()
-    print("NOOO BLOCK")
-    return self.__queue__.get()
+    return h_get_response(self.__queue__, self.__queue_ready__)
 
 
 if __name__ == "__main__":
@@ -115,6 +72,8 @@ if __name__ == "__main__":
   client = myClient(ip="127.0.0.1", port=8081)
   # check validity of user specified arg
   args = parse_arguments(client)
+
   # transmit command
-  x = client.send(args.get)
-  print(x.read())
+  client.send(args.get)
+  x = client.get_response()
+  print(x.RESULTS)
---
+++ drops/helpers.py
@@ -0,0 +1,99 @@
+
+from http.client import HTTPConnection, HTTPResponse
+from multiprocessing import Queue, Semaphore
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ServerResponse:
+    """
+        Object for parsing incomming HTTPResponse from Robot
+    """
+    def __init__(self, httObj: HTTPResponse):
+        response = json.loads(httObj.read())
+        self.TIME = response["Time"]
+        self.STATUS = response["Status"]
+        self.LAST_ID = response["LastID"]
+        self.ERROR_CODE = response["ErrorCode"]
+        self.ERROR_MESSAGE = response["ErrorMessage"]
+        self.RESULTS = response["Results"]
+
+
+class SupportedEndsHandler:
+    """
+        Class ment to handle supported endpoints Json file.
+            Reloads endpoints, keeps track of API args, and possible 'do' actions
+
+            TODO: Write updates to JSON file?
+    """
+    def __init__(self, file : str, conn : HTTPConnection):
+        self.file = file
+        self.__queue__ = Queue()
+        self.__queue_ready__ = Semaphore(value=0)
+        self.__conn__ = conn
+        self.supported_ends = {
+          'get' : [],
+          'do' : {},
+          'conn' : []
+        }
+        self.reload_all()
+
+    def get_endpoints(self):
+        return self.supported_ends
+
+    def reload_endpoint(self, endpoint : str):
+        if endpoint in self.supported_ends['do'].keys():
+          # check this do endpoint takes an argument
+          if '?' in endpoint:
+            # Any float is acceptable for pure moves
+            if 'MoveX' in endpoint or 'MoveY' in endpoint or "MoveZ" in endpoint:
+              return
+            cursed = f"/DoD/get/{endpoint.split('?')[1].split('=')[0]}s"
+            send(self.__conn__, self.__queue__, self.__queue_ready__, cursed)
+            self.supported_ends['do'][endpoint] = get_response(self.__queue__,
+                                                               self.__queue_ready__).RESULTS
+
+    def reload_all(self):
+        try:
+          f = open(self.file)
+        except FileNotFoundError:
+          logger.error("File supported.json not found")
+
+        with f:
+          json_data = json.load(f)["endpoints"]
+          self.supported_ends['get'] = [x['API'] for x in json_data if x['API'][5:8] == 'get']
+          self.supported_ends['do'] = {x['API'] : None for x in json_data if x['API'][5:7] == 'do'}
+          self.supported_ends['conn'] = [x['API'] for x in json_data if 'connect' in x['API'][5:].lower()]
+
+        for ent in self.supported_ends['do'].keys():
+          self.reload_endpoint(ent)
+
+
+'''
+send transmits a formatted HTTP GET request
+it will not check the validity of request
+it will persist result in a place that can be read.
+'''
+def send(conn : HTTPConnection, queue : Queue, q_ready : Semaphore, endpoint : str):
+    logger.info(f"attempting to send: {endpoint}...")
+
+    conn.request("GET", endpoint)
+    logger.info("issued request")
+    reply = conn.getresponse()
+    logger.info("got response")
+
+    reply_obj = ServerResponse(reply)
+
+    if (queue is not None):
+      queue.put(reply_obj)
+      q_ready.release()
+    return
+
+
+'''
+Pops most recent response from response queue
+'''
+def get_response(queue : Queue, q_ready : Semaphore):
+    q_ready.acquire()
+    return queue.get()
