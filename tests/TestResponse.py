b83f990c36b472b1fda7f9d917fe65036e7b4acb
josh-deb <joshc@slac.stanford.edu>
2024-05-08 16:27:06 -0700

    feat(pytest): taking care of some package nuances, enable pytestting of mock server

--- tests/TestResponse.py
+++ tests/TestResponse.py
@@ -1,18 +1,28 @@
-import pytest 
-import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from testServer import MyServer
+from drops.DropsDriver import myClient
+from multiprocessing import Process, Event 
+from http.server import HTTPServer
+
+
+def launch_web_server(event):
+  hostName = "localhost"
+  serverPort = 8081
+  supportedJson = 'drops/supported.json'
+  webServer = HTTPServer((hostName, serverPort), MyServer)
+  print("Server started http://%s:%s" % (hostName, serverPort))
+
+  while (not event.is_set()):
+      webServer.handle_request()
+
+  webServer.server_close()
+  print("Server stopped.")
 
 
 class TestResponse:
   def test_responses(self, capsys):
-    hostName = "localhost"
-    serverPort = 8081
-    supportedJson = '../supported.json'
+    ev = Event()
+    proc = Process(target=launch_web_server, args=(ev,))
+    proc.start()
 
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-    webServer.handle_request()
-
-    webServer.server_close()
-    print("Server stopped.")
+    ev.set()
+    proc.join()
