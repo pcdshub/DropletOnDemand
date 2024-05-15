2e366ba04821af222390a1baaba609fdad016b6a
josh-deb <joshc@slac.stanford.edu>
2024-05-15 16:09:35 -0700

    Josue take the wheel

--- tests/TestHIL.py
+++ tests/TestHIL.py
@@ -14,8 +14,16 @@ json_handler = JsonFileHandler(supported_json)
 # load configs and launch web server 
 json_handler.reload_endpoints()
 
+
+'''
+## Three classes of HIL tests
+1. Trivial, the supported.json has the key, value pair we expect to see on the device
+2. The device value does not equal the value associated with the key in supported.json (because its a live value) and we want to **schema match**
+3. We permutate the state of the device via actuation and we monitor the key value pair assocaited with the actuation to confirm it was successful
+'''
+
 class TestHIL:
-  # Responses in which the values corresponding keys dont matter
+  # Class 1 Test
   def test_disconnect(self, capsys):
     resp = client.disconnect()
     assert resp.RESULTS == json_handler.get_endpoint_data("/DoD/Disconnect")
@@ -27,4 +35,6 @@ class TestHIL:
     resp = client.connect()
     assert resp.RESULTS == json_handler.get_endpoint_data("/DoD/Connect?ClientName={value}")
 
-  
+  # Class 2 Test
+
+  # Class 3 Test
